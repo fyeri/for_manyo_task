32e93c5593d06ffde93365ac160e4c59bcc8f0bc
SAORI KODERA <tibezu.dash@gmail.com>
2024-01-17 18:55:40 +0900

    Genfile編集

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,13 +4,6 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
-        
-        # Task.create!(title: '書類作成', content: '企画書を作成する。')
-        
-        # visit tasks_path
-        
-        # expect(page).to have_content '書類作成'
-        # expect(page).to have_content '企画書を作成する。'
 
       end
     end
@@ -33,12 +26,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it 'そのタスクの内容が表示される' do
-        # Task.create!(title: '書類作成', content: '企画書を作成する。')
-       
-        # visit task_path(task)
 
-        # expect(page).to have_content '書類作成'
-        # expect(page).to have_content '企画書を作成する。'
        end
      end
   end
