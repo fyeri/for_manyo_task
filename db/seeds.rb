17b8476765a43a9492927d28b7b522fbc49fea84
SAORI KODERA <tibezu.dash@gmail.com>
2024-01-18 21:19:37 +0900

    seedは記述した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  Task.create(title: "ブログ記述#{n}回目", content:"#{n}回目")
+end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -17,19 +17,34 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
+       # let!を使ってテストデータを変数として定義することで、複数のテストでテストデータを共有できる
+      let!(:task) { FactoryBot.create(:task, title: 'task_title') }
+ # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+       before do
+          visit tasks_path
+       end
+
     context '一覧画面に遷移した場合' do
-      it '登録済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを登録
-        # Task.create!(title: '書類作成', content: '企画書を作成する。')
-        # タスク一覧画面に遷移
-        FactoryBot.create(:task)
-        visit tasks_path
-        # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
+      it '作成済みのタスク一覧が作成日時の降順で表示される' do
+
+        task_list = all('body tr')
+        
+        expect(page).to 
         expect(page).to have_content '書類作成'
         # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
       end
     end
+     context '新たにタスクを作成した場合' do
+       it '新しいタスクが一番上に表示される' do
+
+
+        expect(page).to task_title = first_task
+        expect(page).to task_title = second_task
+        expect(page).to task_title = third_task
+      end
+    end    
   end
+
 
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
