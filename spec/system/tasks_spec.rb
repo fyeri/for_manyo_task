20653ede30d1cb6a58e63bf65c7574535dcef3e5
SAORI KODERA <tibezu.dash@gmail.com>
2024-01-18 09:51:20 +0900

    FactoryBotを使ってテストデータを作成する課題まで進んだ

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
+        
+        # Task.create!(title: '書類作成', content: '企画書を作成する。')
+        FactoryBot.create(:task)
+        visit tasks_path
+        
+        expect(page).to have_content ''
+        expect(page).to have_content '企画書を作成する。'
 
       end
     end
@@ -13,8 +20,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを登録
-        Task.create!(title: '書類作成', content: '企画書を作成する。')
+        # Task.create!(title: '書類作成', content: '企画書を作成する。')
         # タスク一覧画面に遷移
+        FactoryBot.create(:task)
         visit tasks_path
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
@@ -26,7 +34,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it 'そのタスクの内容が表示される' do
+        # task = Task.create!(title: '書類作成', content: '企画書を作成する。')
 
+        task = FactoryBot.create(:task)
+       
+        visit task_path(task)
+
+        expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
        end
      end
   end
