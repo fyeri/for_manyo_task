29975dacbe0360ccd664b165869ac3b18cdcb03a
SAORI KODERA <tibezu.dash@gmail.com>
2024-01-26 13:25:53 +0900

    現状完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Task.destroy_all
+user = User.create(
+  name: 'user1',
+  email: 'user@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: false
+)
 
-# Seed data
-tasks_data = [
-  { title: 'first_task', content: 'Task content 1', deadline_on: '2025-02-18', priority: '中', status: '未着手' },
-  { title: 'second_task', content: 'Task content 2', deadline_on: '2025-02-17', priority: '高', status: '着手中' },
-  { title: 'third_task', content: 'Task content 3', deadline_on: '2025-02-16', priority: '低', status: '完了' },
-  { title: 'fourth_task', content: 'Task content 4', deadline_on: '2025-02-15', priority: '中', status: '未着手' },
-  { title: 'fifth_task', content: 'Task content 5', deadline_on: '2025-02-14', priority: '高', status: '着手中' },
-  { title: 'sixth_task', content: 'Task content 6', deadline_on: '2025-02-13', priority: '低', status: '完了' },
-  { title: 'seventh_task', content: 'Task content 7', deadline_on: '2025-02-12', priority: '中', status: '未着手' },
-  { title: 'eighth_task', content: 'Task content 8', deadline_on: '2025-02-11', priority: '高', status: '着手中' },
-  { title: 'ninth_task', content: 'Task content 9', deadline_on: '2025-02-10', priority: '低', status: '完了' },
-  { title: 'tenth_task', content: 'Task content 10', deadline_on: '2025-02-09', priority: '中', status: '未着手' }
-]
+admin = User.create(
+  name: 'admin1',
+  email: 'admin@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true
+)
 
-tasks_data.each do |task_data|
-  Task.create!(task_data)
 
+50.times do |n|
+  random_deadline = Date.today + rand(30)
+
+  task = Task.new(
+    title: "やることリスト#{n + 1}日目",
+    content: "課題#{n + 1}個目",
+    deadline_on: random_deadline,
+    priority: Task.priorities.keys.sample,
+    status: Task.statuses.keys.sample,
+    user: user
+  )
+  task.save
+end
+
+50.times do |n|
+  random_deadline = Date.today + rand(30)
+
+  task = Task.new(
+    title: "タスクリスト#{n + 1}日目",
+    content: "テスト#{n + 1}個目",
+    deadline_on: random_deadline,
+    priority: Task.priorities.keys.sample,
+    status: Task.statuses.keys.sample,
+    user: admin
+  )
+  task.save
 end
